3c756f1a8bb7b38733d0ec4b2304648b08574246
thruxuri <thruxzxn@gmail.com>
2024-11-22 02:29:28 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let text = 'Rafaela Quisigüiña';
-let h2 = document.getElementById('Rafaela Quisigüiña');
-
-text.split('').forEach((char,index) => {
-    let span = document.createElement('span');
-    span.textContent = char === ' ' ? '\u00A0\u00A0' : char;
-    span.style.transitionDelay = `${(index % 10) * 0.1}s`
-    h2.appendChild(span);
-})
