a50c0f1dd17de66ebfe8e96da52a311cb244e99d
thruxuri <thruxzxn@gmail.com>
2024-12-07 06:18:56 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let text = 'natapafresa';
-let h2 = document.getElementById('natapafresa');
-
-text.split('').forEach((char,index) => {
-    let span = document.createElement('span');
-    span.textContent = char === ' ' ? '\u00A0\u00A0' : char;
-    span.style.transitionDelay = `${(index % 10) * 0.1}s`
-    h2.appendChild(span);
-})
